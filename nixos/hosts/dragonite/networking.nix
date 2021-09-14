731251942ca7f53eff94c54f998ff79f0bc67237
jayrovacsek <jay@rovacsek.com>
2021-09-15 08:35:05 +1000

    Add port for mistio testing

--- nixos/hosts/dragonite/networking.nix
+++ nixos/hosts/dragonite/networking.nix
@@ -8,7 +8,7 @@
   };
 
   networking.firewall = {
-    allowedTCPPorts = [ 22 80 139 443 445 8200 9000 ];
+    allowedTCPPorts = [ 22 80 139 443 445 7200 8200 9000 ];
     allowedUDPPorts = [ 137 138 ];
   };
 }
