ee7320fcfc04df7b78b659cce63af97ff9679ce6
jayrovacsek <jay@rovacsek.com>
2021-09-15 08:36:07 +1000

    Start shelling out my own approach to qemu guests

--- nixos/hosts/dragonite/virt/meta.nix
+++ nixos/hosts/dragonite/virt/meta.nix
@@ -1,6 +1,11 @@
-{
+{ config, lib, pkgs, ... }: {
   virtualisation.docker = {
     enable = true;
     enableNvidia = true;
   };
+
+  virtualisation.libvirtd.onBoot = "start";
+  virtualisation.libvirtd.onShutdown = "shutdown";
+  virtualisation.libvirtd.qemuPackage = pkgs.qemu_kvm;
+  virtualisation.libvirtd.enable = true;
 }
